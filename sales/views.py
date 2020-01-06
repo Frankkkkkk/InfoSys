49b4337cdfe0a9dbc8e001553d928d53311bbd7d
Frank Huo <>
2020-01-06 23:04:31 +0800

    url commit

--- sales/views.py
+++ sales/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+
 
 def listorders(request):
     return HttpResponse("下面是系统中所有的订单信息。。。")
+
+def listorders1(request):
+    return HttpResponse("下面是系统中所有的订单信息。。。1")
+
+def listorders2(request):
+    return HttpResponse("下面是系统中所有的订单信息。。。2")
+
+def listorders3(request):
+    return HttpResponse("下面是系统中所有的订单信息。。。3")
+
